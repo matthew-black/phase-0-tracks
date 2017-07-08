dfd83cacf941aec91ef5fcd5f942d935d3f4dee7
matthew-black <matthewallenblack@gmail.com>
2017-07-08 16:32:50 -0600

    Add user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -84,11 +84,90 @@ alias_string
 end
 
   # Test calls to make sure all the above stuff works!
-    names_string = first_last_swap("Matthew Black")
-    puts name_to_alias(names_string)
-    names_string = first_last_swap("Felicia Torres")
-    puts name_to_alias(names_string)
+    # names_string = first_last_swap("Matthew Black")
+    # puts name_to_alias(names_string)
+    # names_string = first_last_swap("Felicia Torres")
+    # puts name_to_alias(names_string)
 
+# Method that flips the order of first name and
+# last name when provided in a single string:
+def first_last_swap (full_name_string)
+    swapped_full_name_array = full_name_string.split(' ').rotate(1)
+    swapped_full_name_string = swapped_full_name_array.join(' ')
+end
+
+
+# Method that changes the name string into
+# an alias:
+def name_to_alias(string)
+  # All the variables/arrays required by the
+  # if/elsif statements:
+  vowels_downcase = "aeiou"
+  vowels_downcase_array = vowels_downcase.split('')
+  vowels_upcase = "AEIOU"
+  vowels_upcase_array = vowels_upcase.split('')
+  consonants_downcase = "bcdfghjklmnpqrstvwxyz"
+  consonants_downcase_array = consonants_downcase.split('')
+  consonants_upcase = "BCDFGHJKLMNPQRSTVWXYZ"
+  consonants_upcase_array = consonants_upcase.split('')
+  # The logic stuff that builds a new string
+  # based on the specs for 5.5:
+  alias_string = ""
+  count = 0
+  while count < string.length
+    if string[count] == " "
+      alias_string[count] = " "
+      count +=1
+    elsif vowels_downcase_array.include? string[count]
+      converted = vowels_downcase.index(string[count]) + 1
+      alias_letter = vowels_downcase[converted]
+      alias_string[count] = alias_letter
+      count += 1
+    elsif consonants_downcase_array.include? string[count]
+      converted = consonants_downcase.index(string[count]) + 1
+      alias_letter = consonants_downcase[converted]
+      alias_string[count] = alias_letter
+      count += 1
+    elsif vowels_upcase_array.include? string[count]
+      converted = vowels_downcase.index(string[count]) + 1
+      alias_letter = vowels_upcase[converted]
+      alias_string[count] = alias_letter
+      count += 1
+    elsif consonants_upcase_array.include? string[count]
+      converted = consonants_upcase.index(string[count]) + 1
+      alias_letter = consonants_upcase[converted]
+      alias_string[count] = alias_letter
+      count += 1
+    else
+      alias_string[count] = " "
+    end
+  end
+alias_string
+end
+
+  # Test calls to make sure all the above stuff works!
+    # names_string = first_last_swap("Matthew Black")
+    # puts name_to_alias(names_string)
+    # names_string = first_last_swap("Felicia Torres")
+    # puts name_to_alias(names_string)
+
+# User interface:
+puts "Welcome to the world's most advanced alias creation program."
+puts "Please type a first and last name. Then, press enter to receive an alias."
+puts "Type 'quit' when you've finished creating aliases."
+not_done = true
+while not_done
+  print "First and last name: "
+  user_response = gets.chomp
+  if user_response.downcase == "quit"
+    puts "Thanks for using the world's most advanced alias creation program."
+    not_done = false
+  else
+    names_string = first_last_swap(user_response)
+    alias_string = name_to_alias(names_string)
+    puts "#{user_response}'s alias is #{alias_string}"
+  end
+end
 
 
 
