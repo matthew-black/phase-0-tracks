461b387d005bcf46289e06e88683245d035ae97a
matthew-black <matthewallenblack@gmail.com>
2017-07-09 13:00:43 -0600

    Make fibonacci method, spend some internet time with sorting algorithms

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,6 +1,7 @@
-# Search an array for a certain integer,
-# if found, return its index position(s).
-# If no integer matches, return nil.
+ # RELEASE 0
+  # Searches an array for a certain integer.
+  # If found, returns its index position(s).
+  # If no integer matches, returns nil.
 def array_search(array, integer)
   count = 0
   stored_index_matches = []
@@ -25,11 +26,35 @@ end
     # p array_search(arr, 23)
     # p array_search(arr, 5)
 
-# Calculate numbers in the Fibonacci sequence
+# RELEASE 1
+  # Calculates a specified amount of numbers from
+  # the Fibonacci sequence.
 def fibonacci(how_many_numbers)
   fibonacci_sequence = []
   count = 0
   while count < how_many_numbers
-
+    if count == 0
+      fibonacci_sequence << 0
+      count += 1
+    elsif count == 1
+      fibonacci_sequence << 1
+      count += 1
+    else
+      fibonacci_sequence << fibonacci_sequence[-1] + fibonacci_sequence[-2]
+      count += 1
+    end
+  end
+  fibonacci_sequence
 end
+  # Test calls for the above method:
+    # p fibonacci(10)
+    # fib_test = fibonacci(100)
+    # p fib_test[-1] == 218922995834555169026 #<--should return true
 
+# RELEASE 2
+  # Pseudocode:
+  #
+  #
+  #
+def array_sort
+end
