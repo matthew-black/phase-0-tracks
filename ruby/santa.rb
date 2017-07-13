2d99e472439c1d22b077fefa29ef76051329db21
matthew-black <matthewallenblack@gmail.com>
2017-07-13 12:48:19 -0600

    Initial commit

---
+++ ruby/santa.rb
@@ -0,0 +1,23 @@
+#--RELEASE 6.3: ATTRIBUTES--#
+
+class Santa
+
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+
+  def speak
+    puts "Ho, ho, ho! Happy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+end
+
+  # #--Test calls--#
+  #   steve = Santa.new
+  #   steve.speak
+  #   steve.eat_milk_and_cookies("snickerdoodle")
+
