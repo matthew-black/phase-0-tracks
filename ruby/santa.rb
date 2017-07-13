36025ba826539031d77d846620d2ccf8de41fd2d
matthew-black <matthewallenblack@gmail.com>
2017-07-13 16:06:12 -0600

    Make santa.age random for each instance, print out all santa instances with age, ethnicity, and gender attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,16 +1,26 @@
 #----RELEASE 6.3: ATTRIBUTES---#
 
-  #--Creates the Santa class--#
+  #--Creates and adds attributes to the Santa class--#
 class Santa
-  attr_reader :age, :ethnicity
-  attr_accessor :gender
+  attr_reader :ethnicity
+  attr_accessor :age, :gender
 
   def initialize(gender, ethnicity)
-    puts "Initializing Santa instance..."
+    # puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
+  end
+
+  def about
+    puts "  Age == #{@age}"
+    puts "  Ethnicity == #{@ethnicity}"
+    puts "  Gender == #{@gender}"
+  end
+
+  def set_random_age
+    @age = rand(0..140)
   end
 
   def celebrate_birthday
@@ -22,7 +32,7 @@ class Santa
   end
 
   def eat_milk_and_cookies(cookie_type)
-    puts "That was a good #{cookie_type}!"
+    puts "That was a good #{cookie_type} cookie!"
   end
 
   def get_mad_at(reindeer_name)
@@ -33,28 +43,27 @@ end
   #   steve = Santa.new("male", "Belgian")
   #   steve.speak
   #   steve.eat_milk_and_cookies("snickerdoodle")
-    # #--Test calls for attribute-changing and getter/setter methods--#
-      betsy = Santa.new("female", "Icelandic")
-      p betsy.celebrate_birthday
-      p betsy.get_mad_at("Rudolph")
-      p betsy.gender = "robot"
-      p betsy.age
-      p betsy.ethnicity
+  # #--Test calls for attribute-changing and getter/setter methods--#
+  #   p betsy = Santa.new("female", "Icelandic")
+  #   p betsy.celebrate_birthday
+  #   p betsy.get_mad_at("Rudolph")
+  #   p betsy.gender = "robot"
+  #   p betsy.age
+  #   p betsy.ethnicity
 
 
     #--Creates a specified number of random Santas--#
 def santa_maker(number_of_santas)
   santa_factory = []
-  possible_genders = ["agender", "female", "bigender", "male", "robot", "gender fluid", "N/A"]
-  possible_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-  number_of_santas.times  {santa_factory << Santa.new(possible_genders[rand(0..6)], possible_ethnicities[rand(0..6)])}
+  possible_genders = ["Agender", "Female", "Bigender", "Male", "Robot", "Gender Fluid", "N/A"]
+  possible_ethnicities = ["Black", "Latino", "White", "Japanese-African", "Prefer Not to Say", "Mystical Creature (Unicorn)", "N/A"]
+  number_of_santas.times  {santa_factory << Santa.new(possible_genders.sample, possible_ethnicities.sample)}
   santa_factory
 end
-  # #--Test calls for santa_maker method--#
-  #   santas = santa_maker(3)
-  #   p santas
-
-
-
-
-
+  # #--Test calls for santa_maker method, #set_random_age, and #about--#
+    # santa_array = santa_maker(5)
+    # santa_array.each { |santa_instance| santa_instance.set_random_age}
+    # santa_array.size.times do |i|
+    #   puts "Santa ##{i + 1}:"
+    #   print "#{santa_array[i].about}"
+    # end
