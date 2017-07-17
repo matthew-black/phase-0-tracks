67f5db9c496569e2510b89cb3ba76d36d0e62d6f
matthew-black <matthewallenblack@gmail.com>
2017-07-17 16:20:03 -0600

    Write pseudocode for string reversing function

---
+++ js/explore.js
@@ -0,0 +1,20 @@
+// For a provided string:
+// string.length - 1 will equal the indexed value
+// of the final character in the string
+
+// I just need to find a Javascript function that
+// works like Ruby's "print" instead of Ruby's "puts".
+// "console.log" seems to function like "puts".
+
+// Once I've found that function, it'll just be like making
+// the loop that counted backward from 10 to 1.
+
+// So, set a variable to string.length - 1, which will make
+// the first iteration of the loop start with the final
+// letter of the provided string.
+
+// Make the loop logic be while this variable is greater
+// than or equal to 0, decrease the variable by 1 ("x--").
+
+// With each iteration of the loop, print the character
+// that is returned, without starting a new line.
