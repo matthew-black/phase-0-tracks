f82f2a78fdfdf188c4f67bad8f9211af4638a4f8
matthew-black <matthewallenblack@gmail.com>
2017-07-21 15:45:12 -0600

    Refactor virus methods to be more readable

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
 # I worked on this challenge [by myself, with: Marla Kallstrom and Davina].
-# We spent [5,000] hours on this challenge.
+# We spent [1.2] hours on this challenge.
 
 # EXPLANATION OF require_relative
 #---It is used to link a file that is in the same folder
@@ -17,53 +17,49 @@ class VirusPredictor
   end
     #--Literally just calling predicted_deaths and speed_of_spread,
     #--probz just to simplify the user interface code.
+
   def virus_effects
-    predicted_deaths#(@population_density, @population, @state)
-    speed_of_spread#(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
     #--Calculates how many people are gonna die, based on
     #--the total population multiplied by a population density
     #--variable.
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      multiplier = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      multiplier = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      multiplier = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      multiplier = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      multiplier = 0.05
     end
-
+    number_of_deaths = (@population * multiplier).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
     #--Based on population density, this method just states, in months,
     #--how long it will take for the disease to spread.
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
-
     puts " and will spread across the state in #{speed} months.\n\n"
-
   end
 
 end
