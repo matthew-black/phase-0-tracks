59133b7f2f8b9405884ad5e3a8dd8724c74f79df
matthew-black <matthewallenblack@gmail.com>
2017-07-23 19:42:12 -0600

    Create a function that generates an array containing a specified number of fake words

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,8 @@
-// 7.3 Solo Challenge: Algorithm Practice
+//--7.3 Solo Challenge: Algorithm Practice--//
 
 
-// #0. Pseudocode
+
+//--#0. Pseudocode--//
   //  Send the array of words/phrases into the function.
   //  Define a new variable inside the function that is equal to "a".
   //  Start a loop with a count of 0.
@@ -20,15 +21,16 @@ function findTheLongestString(array) {
   }
   return isLongest;
 }
-  //--The driver code!
+  //--Driver Code:
   var states = ["Nevada", "Texas", "Utah", "Alaska", "Rhode Island", "Massachusettes", "North Dakota"];
   var dogs = ["Chow", "Corgi", "Labrador", "Pug", "Golden Retriever", "Belgian Malinois"];
-//--Driver Code / Test Calls:
+  //-Test Calls
   console.log(findTheLongestString(states));
   console.log(findTheLongestString(dogs));
 
 
-// #1. Pseudocode
+
+//--#1. Pseudocode--//
   //  Make a function that takes object1 and object2 as its arguments.
   //  Will need to find a way to figure out how many key-value pairs are
   //  part of an object.
@@ -55,7 +57,7 @@ function findTheLongestString(array) {
   // match was found, used those arrays to access the value to check for
   // equality.
 
-//--Function!
+//--Function:
 function keyValuePairChecker(object1, object2) {
   isMatch = false
   keys1 = Object.keys(object1)
@@ -71,11 +73,50 @@ function keyValuePairChecker(object1, object2) {
   }
   return isMatch
 }
-//--Driver Code / Test Calls!
-var dog = {name: "Henry", age: 7, color: "Brown", legs: 4};
-var monkey = {name: "Josephine", age: 3, color: "Brown", legs: 2, tail: true};
-var spider = {name: "Pauline", age: 14, color: "Green", legs: 8};
+  //--Driver Code:
+  var dog = {name: "Henry", age: 7, color: "Brown", legs: 4};
+  var monkey = {name: "Josephine", age: 3, color: "Brown", legs: 2, tail: true};
+  var spider = {name: "Pauline", age: 14, color: "Green", legs: 8};
+  //--Test Calls:
+  console.log(keyValuePairChecker(dog, monkey)); //-should return true
+  console.log(keyValuePairChecker(dog, spider)); //-should return false
 
-console.log(keyValuePairChecker(dog, monkey)); //-should return true
-console.log(keyValuePairChecker(dog, spider)); //-should return false
 
+
+//--#2. Pseudocode--//
+  // Create a function that takes an integer as an argument.
+  // Create a variable that houses the alphabet.
+  // Create an empty array to house the random strings.
+  // Set a loop to run as many times as the integer argument.
+  // Figure out what the "random" syntax is for JavaScript:
+    // Looking for something like Ruby's <rand(10)> method.
+  // For each iteration of the loop, set a random length for
+  // a string, and populate it with random letters from alphabet variable.
+  // Then, add the new string to the empty array, like "fakeWords[x] = newWord",
+  // or whatever variables you use. (The "x" in "fakeWords[x]" will need to be
+  // whatever "counter" variable you use for the outermost loop.)
+
+//--Function:
+function makeFakeWords(integer) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz"; //26 letters, derp
+  var fakeWords = [];
+  for (var x = 0; x < integer; x++) {
+    var howManyChars = Math.floor((Math.random() * 10) + 1); //-The "+1" makes it
+    var word = "";                                           //-generate 1 through 10,
+    for (var y = 0; y < howManyChars; y++) {                 //-rather than 0 through 9.
+      word += alphabet[Math.floor((Math.random() * 26))];
+    }
+    fakeWords[x] = word;
+  }
+  return fakeWords;
+}
+
+//--Driver Code:
+for (var x = 0; x < 10; x++) {
+  var array = makeFakeWords(5);
+  console.log("An array of randomness:")
+  console.log(array);
+  console.log("The longest word in this array is:")
+  console.log(findTheLongestString(array));
+  console.log(" ")
+}
